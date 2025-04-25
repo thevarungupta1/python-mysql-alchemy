84fd8cd1266e3a6da679abbc701a94eddce2d8d3
Varun Gupta <thevarungupta1@gmail.com>
2025-04-26 01:20:00 +0530

    initial commit

--- database.py
+++ database.py
@@ -1,15 +1,20 @@
 import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# we can also set these environment variables 
-# DB_USER = os.getenv("DB_USER", "root")
-# DB_PASS = os.getenv("DB_PASS", "root")
-# DB_HOST = os.getenv("DB_HOST", "localhost")
-# DB_NAME = os.getenv("DB_NAME", "test_db")
+# load the .env file
+load_dotenv()
 
-#DATABASE_URL = f"mysql+pymysqlconnector://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
-DATABASE_URL = f"mysql+mysqlconnector://root:root@localhost/test_db"
+# we can also set these environment variables 
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASS = os.getenv("DB_PASS", "root")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_NAME = os.getenv("DB_NAME", "test_db")
+
+
+DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
+#DATABASE_URL = f"mysql+mysqlconnector://root:root@localhost/test_db"
 
 # Create the SQLAlchemy engine
 engine = create_engine(DATABASE_URL, echo=True)
